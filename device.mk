c80e225a5fc37192aa2a43493d5c73914b54d065
JYRRC <javixhtsweet@gmail.com>
2024-05-19 21:10:51 +0400

    sweet_k6a: Opt for SCUDO and bypass jemalloc
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>
    Signed-off-by: eklerismunir <munir.nasibzade@proton.me>

--- device.mk
+++ device.mk
@@ -483,6 +483,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Scudo
+PRODUCT_USE_SCUDO := true
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
