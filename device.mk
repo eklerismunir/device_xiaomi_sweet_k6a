246b307649e026528f94b7eb95a8f6f4f8c407a6
eklerismunir <munir.nasibzade@proton.me>
2024-05-21 21:23:28 +0400

    sweet_k6a: Drop ViPER4AndroidFX
    - Time to drop 4c4825a we dont need ooga booga things
    Signed-off-by: eklerismunir <munir.nasibzade@proton.me>

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit from ViPER4AndroidFX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
